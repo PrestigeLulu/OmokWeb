4913f1b678b4253ac1ce148142c50d33fbdafa9b
MIN <alsrb4171@gmail.com>
2024-11-04 13:39:15 +0900

    a

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
 
 const server = http.createServer(app);
